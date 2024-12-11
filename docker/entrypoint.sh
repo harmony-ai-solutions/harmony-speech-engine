741aca121a9696dd7c8412a11f54e650ede2bc7b
RuntimeRacer <sge2022@gmail.com>
2024-12-12 00:43:33 +0100

    Update Docker Setup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,6 @@ echo 'Starting Harmony Speech Engine API server...'
 CMD="python3 -m harmonyspeech.endpoints.openai.api_server
              --host 0.0.0.0
              --port 12080
-             --download-dir ${HF_HOME:?}/hub
              ${CMD_ADDITIONAL_ARGUMENTS}"
 
 # set umask to ensure group read / write at runtime
